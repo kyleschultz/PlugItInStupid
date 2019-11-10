1bcbb817c5dc6819091b9dd6b1a7dc4f7894c048
Kyle Schultz <kyle.j.schultz23@gmail.com>
2019-11-10 14:18:41 -0700

    wut

--- app/src/main/java/com/example/myapplication/AddAlarm.java
+++ app/src/main/java/com/example/myapplication/AddAlarm.java
@@ -3,12 +3,21 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TimePicker;
+import java.util.Calendar;
 
 public class AddAlarm extends AppCompatActivity {
 
+    private TimePicker tPicker;
+    private int hour;
+    private int minute;
+    private Calendar calendar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_alarm);
+        tPicker = (TimePicker) findViewById(R.id.timePicker);
+        calendar = calendar.getInstance();
     }
 }
+
