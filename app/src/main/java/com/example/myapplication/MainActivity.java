baf6c04d6b707f5c36b3b988ead9d8bb0d9d6f1d
Kyle Schultz <kyle.j.schultz23@gmail.com>
2019-11-10 13:49:16 -0700

    Add alarm activity

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView button = findViewById(R.id.addAlarm);
+        //button.setOnClickListener({});
     }
+
+
+
 }
