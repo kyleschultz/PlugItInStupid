52ea88913d9021ee6ae54534541d51503c315219
Aaron Steiner <36575740+SnowyWolf7@users.noreply.github.com>
2019-11-11 13:32:18 -0700

    added needed activity and xml files

---
+++ app/src/main/java/com/example/myapplication/ChangeTimeZone.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChangeTimeZone extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_time_zone);
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/TimerScreen.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TimerScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timer_screen);
+    }
+}
