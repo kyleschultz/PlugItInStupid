5506fce3b1bd9ca5ba7899c0c8b87d599162419d
Pari <pari.bhetwal@gmail.com>
2019-11-13 21:14:37 -0700

    nice

--- app/src/main/java/com/example/myapplication/ChangeTimeZone.java
+++ app/src/main/java/com/example/myapplication/ChangeTimeZone.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ChangeTimeZone extends AppCompatActivity {
+public class ChangeTimeZone extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.Image;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.content.Intent;
 import android.view.View;
@@ -21,27 +22,46 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
         this.context = this;
+        // Add alarm
         ImageView addButton = findViewById(R.id.addAlarm);
         addButton.setOnClickListener(this);
+
+        // Ringtone
         ImageView toneButton = findViewById(R.id.imageView3);
         toneButton.setOnClickListener(this);
+
+        // Count-down timer
         ImageView timerButton = findViewById(R.id.countdownTimer);
         timerButton.setOnClickListener(this);
 
+        // Time-zone
+        Button tzButton = findViewById(R.id.timeZoneButton);
+        tzButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
+        // Add alarm
         if(v.getId() == R.id.addAlarm) {
             Intent myintent = new Intent(MainActivity.this, AddAlarm.class);
             startActivity(myintent);
         }
+
+        // Count-down timer
         else if(v.getId() == R.id.countdownTimer){
             Intent myintent = new Intent(MainActivity.this, TimerScreen.class);
             startActivity(myintent);
         }
+
+        // Ring-tone
         else if(v.getId() == R.id.imageView3){
             Intent myintent = new Intent(MainActivity.this, MusicSelection.class);
+            startActivity(myintent);
+        }
+
+        // Time-zone
+        else if(v.getId() == R.id.timeZoneButton){
+            Intent myintent = new Intent(MainActivity.this, ChangeTimeZone.class);
             startActivity(myintent);
         }
 
