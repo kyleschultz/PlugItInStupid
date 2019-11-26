a9b11b77677606af43efc73bd6536d6687679eb9
Aaron Steiner <36575740+SnowyWolf7@users.noreply.github.com>
2019-11-26 12:42:47 -0700

    Added mp3 files and am working on incorporating them with the music selection file

--- app/src/main/java/com/example/myapplication/MusicSelection.java
+++ app/src/main/java/com/example/myapplication/MusicSelection.java
@@ -2,7 +2,10 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 
 public class MusicSelection extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MusicSelection extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music_selection);
+    }
+
+
+    MediaPlayer mMediaPlayer;
+
+    // Controls what happens upon clicking on features
+
+    public void onClick(View v) {
+        // Add alarm
+        if (v.getId() == R.id.ringtone1) {
+            Intent myintent = new Intent(MusicSelection.this, MusicSelection.class);
+            //startActivity(myintent);
+            mMediaPlayer = MediaPlayer.create(this, R.raw.toxicity);
+        }
     }
 }
